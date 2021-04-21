b9eae641867652d279c4102fe8c48379a7fbd110
Oliwier-Gebczynski <oliwiergebczynski@gmail.com>
2021-04-21 13:36:21 +0200

    skończony

---
+++ Egzamin/skrypt.js
@@ -0,0 +1,31 @@
+const btn = document.querySelector("#przycisk");
+
+btn.addEventListener("click", function(){
+    const radioInputs = document.querySelectorAll(".input");
+    let checkNumber = 0; 
+
+    radioInputs.forEach((input) =>{
+        checkNumber++
+
+        if(input.checked){
+            const wynik = document.querySelector("#cenaWynik");
+            switch (checkNumber){
+                case 1:
+                    wynik.innerText = "25";
+                    break;
+                case 2:
+                    wynik.innerText = "30";
+                    break;
+                case 3:
+                    wynik.innerText = "40";
+                    break;
+                case 4:
+                    wynik.innerText = "50";
+                    break;
+                default:
+                    console.log("nie działa");
+                    break;
+            } 
+        }
+    })
+})
